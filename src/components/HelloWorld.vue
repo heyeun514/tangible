3672748299685b75701dd3c4bcbd0adc72ff603f
hyeeun park <h514.park@samsung.com>
2020-03-18 19:57:54 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,104 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div v-for="item in 196" class="circleBox" v-bind:key="item">
+      <div class="shadow"></div>
+      <div class="circle"></div>
+    </div>
   </div>
 </template>
 
 <script>
+// import gsap from "gsap";
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  methods: {
+    move: function() {
+      // gsap.to()
+    }
+  },
+  mounted() {
+    this.move();
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  background: #81c9c8;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.circleBox {
+  /* display: flex; */
+  position: relative;
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  /* justify-content: center; */
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.circle {
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  width: 80px;
+  height: 80px;
+  border-radius:50%;
+  /* background: #e44d42; */
+  border: 1px solid red;
+  animation: moveUp 2s infinite;
+  animation-timing-function: ease-in;
 }
-a {
-  color: #42b983;
+.shadow {
+  position: absolute;
+  left: 12px;
+  top: 12px;
+  width: 76px;
+  height: 76px;
+  border-radius: 50%;
+  /* background: #369098; */
+  border: 1px solid;
+  /* animation: moveDown 2s infinite; */
+  /* animation-timing-function: ease-in; */
 }
+
+.up {
+  transform: translate(-10px);
+}
+
+@keyframes moveUp {
+  0% {
+    transform: translate(0, 0);
+  }
+  50% {
+    transform: translate(-10px, -10px)
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+}
+
+@keyframes moveDown {
+  0% {
+    transform: translate(0, 0);
+  }
+  50% {
+    transform: translate(10px, 10px)
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+}
+
+
+
 </style>
