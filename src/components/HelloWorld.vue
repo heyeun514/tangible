05294b79f56171488899d6a7e5a834c230ab3dc0
hyeeun <heyeun514@gmail.com>
2020-03-19 01:25:10 +0900

    make canvas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,111 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <canvas id="c" ref="c">
+    </canvas>
   </div>
 </template>
 
 <script>
+const circleW = 50,
+      circleH = 50,
+      circleBoxW = 100,
+      circleBoxH = 100;
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  mounted() {
+    this.$refs.c.width = window.innerWidth;
+    this.$refs.c.height = window.innerHeight;
+    this.start();
+  },
+  methods: {
+    start: function() {
+      var row = window.innerHeight / circleW;
+      var col = window.innerWidth / circleH;
+      var ctx = this.$refs.c.getContext('2d');
+
+      // const moveUpRange = 10;
+
+      function CircleItem(x, y, bx, by, r, w) {
+        this.oriX = x;
+        this.oriY = y;
+        this.bx = bx;
+        this.by = by;
+        this.w = w;
+        this.r = r;
+        this.first = {
+          x: x,
+          y: y,
+          dx: -1,
+          dy: -1,
+        };
+        // first circle update
+        this.update = function() {
+          this.first.x += this.first.dx;
+          this.first.y += this.first.dy;
+          if ((this.first.dx < 0 && this.first.x - r <= this.bx) || // check edge
+             (this.first.dx > 0 && this.first.x >= this.oriX)) {
+              this.first.dx *= -1;
+              this.first.dy *= -1;
+          }
+        }
+      }
+
+      var circlePosition = [];
+      for(var i = 0 ; i <= col; i++) {
+        circlePosition[i] = [];
+        for(var j =0; j <= row; j++) {
+          circlePosition[i].push(new CircleItem(
+                              j*circleBoxW + circleBoxW/2,
+                              i*circleBoxH - circleBoxW/2,
+                              j*circleBoxW,
+                              i*circleBoxH,
+                              circleW / 2,
+                              circleBoxW));
+        }
+      }
+
+      console.log(circlePosition);
+
+      function draw() {
+
+        ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+        for(var i = 0 ; i <= col; i++) {
+          for(var j =0; j <= row; j++) {
+            ctx.beginPath();
+            ctx.arc(circlePosition[i][j].first.x, circlePosition[i][j].first.y, circlePosition[i][j].r, 0, 2*Math.PI);
+
+
+
+            ctx.rect(j*circleBoxW, i*circleBoxH, circleBoxW, circleBoxH);
+
+
+            ctx.strokeStyle = "white";
+            ctx.stroke();
+            circlePosition[i][j].update();
+          }
+        }
+        requestAnimationFrame(draw);
+      }
+
+      requestAnimationFrame(draw);
+      
+
+    }
+  },
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#c {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
