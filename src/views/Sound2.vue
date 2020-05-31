e58c3078296a312795dbcc61cef4b6bd1735da23
hyeeun <heyeun514@gmail.com>
2020-05-31 22:57:14 +0900

    sound2 btn

--- src/views/Sound2.vue
+++ src/views/Sound2.vue
@@ -3,10 +3,11 @@
         <div v-for="(item, index) in 128"
             ref="soundItem"
             v-bind:key="index"
+            v-on:click="push"
             class="sound-item">
         </div>
         <audio ref="music" src="../assets/needed.mp3" controls></audio>
-        <button class="playBtn" v-on:click="playMusic">play</button>
+        <div class="playBtn" v-on:click="playMusic">play</div>
     </div>
 </template>
 <script>
@@ -35,7 +36,8 @@ export default Vue.extend({
                     a[j] = x;
                 }
             },
-            playMusic: function() {
+            playMusic: function(event) {
+            event.target.style.visibility = "hidden";
             var audio = this.$refs.music;
             audio.play();
             audioCtx = new AudioContext();
@@ -56,50 +58,48 @@ export default Vue.extend({
                 orderArray[i] = i;
             }
             this.shuffle(orderArray);
-            // console.log('orderArray', orderArray);
-
             for (var i=0; i<bufferLength; i++) {
                 parseArray[i] = 0;
             }
             this.start();
         },
+        push: function(event) {
+            console.log('el', event.target.key);
+        },
         start: function() {
             var soundItems = this.$refs.soundItem;
             console.log('soundItem', soundItems.length);
             var fr = 0;
+            var shadowVal = [];
+            for(var i =0; i<dataArray.length; i++) {
+                shadowVal[i] = -8;
+            }
             function animation() {
-                // console.log('a');
-                // fr++;
-                // if (fr < 0) return;
-                // else if (fr >= 10)
-                //     fr = 0;
                 if (analyser) {
                     analyser.getByteFrequencyData(dataArray);
                 }
                 for(var i =0; i<dataArray.length; i++) {
                     var c = dataArray[orderArray[i]];
-                    // console.log(c);
-                    var shadowVal = Math.round(easing.linear(c/FRAME, c, -8, 28, FRAME));
+                    var newVal = easing.linear(c/FRAME, c, -8, 28, FRAME);
 
-
-                    if (shadowVal > 0) {
-                        // gsap.to(soundItems[i], 0.01, {
-                        //     boxShadow: `${shadowVal}px ${shadowVal}px ${shadowVal+5}px #cbced1,
-                        //         -${shadowVal}px -${shadowVal}px ${shadowVal+5}px #ffffff`,
-                        // });
-                        soundItems[i].style.boxShadow = `${shadowVal}px ${shadowVal}px ${shadowVal+5}px #cbced1,
-                                 -${shadowVal}px -${shadowVal}px ${shadowVal+5}px #ffffff`;
-                    } else {
-                        shadowVal = Math.abs(shadowVal);
-                        // gsap.to(soundItems[i], 0.01, {
-                        //     boxShadow: `inset ${shadowVal}px ${shadowVal}px ${shadowVal}px #cbced1,
-                        //         inset -${shadowVal}px -${shadowVal}px ${shadowVal}px #ffffff`,
-                        // });
-                        soundItems[i].style.boxShadow = `inset ${shadowVal}px ${shadowVal}px ${shadowVal}px #cbced1,
-                                inset -${shadowVal}px -${shadowVal}px ${shadowVal}px #ffffff`
+                    if (shadowVal[i] < newVal) {
+                        shadowVal[i] = newVal;
+                    } else if (shadowVal[i] > -8) {
+                        shadowVal[i]--;
                     }
 
-                    
+                    var sv = shadowVal[i];
+                    if (i == 10) console.log(sv);
+                    if (sv == -8) {
+                        soundItems[i].style.boxShadow = '';
+                    } else if (sv > 0) {
+                        soundItems[i].style.boxShadow = `${sv}px ${sv}px ${sv+5}px #cbced1,
+                                 -${sv}px -${sv}px ${sv+5}px #ffffff`;
+                    } else if (sv < 0) {
+                        sv = Math.abs(sv);
+                        soundItems[i].style.boxShadow = `inset ${sv}px ${sv}px ${sv}px #cbced1,
+                                inset -${sv}px -${sv}px ${sv}px #ffffff`
+                    }
                 }
                 rAF = requestAnimationFrame(animation);
             }
@@ -136,11 +136,15 @@ export default Vue.extend({
     /* padding: 60px 35px 35px 35px; */
     border-radius: 50%;
     background: #ecf0f3;
-    box-shadow: 13px 13px 20px #cbced1, -13px -13px 20px #ffffff;
+    /* box-shadow: 13px 13px 20px #cbced1, -13px -13px 20px #ffffff;/*
     /* box-shadow: inset 8px 8px 8px #cbced1, inset -8px -8px 8px #ffffff; */
 }
 
 .playBtn {
-    position: absolute;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
 }
 </style>
